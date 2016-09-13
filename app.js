777f6ee1d5699bb9566eb1c09672dfb0797b7db7
Tao Ning <ningtaous@gmail.com>
2016-09-12 20:52:29 -0400

    new feature, able to view homepage without specify /Index.html

--- app.js
+++ app.js
@@ -9,31 +9,14 @@ var staticPages = require('./routes/staticPages');
 var cors = require('cors');
 var app = express();
 
-// view engine setup
-var hbs = exphbs.create({
-  defaultLayout: 'static',
-  partialsDir: 'views/partials/',
-  helpers: {
-    cdnPrefix: function() {
-      return process.env.TAONINGJS_CDNURL;
-    }
-  }
-});
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 app.use(logger('dev'));
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', staticPages);
+app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  res.render('pages/404');
+  res.send('page not found');
 });
 
 // error handlers
